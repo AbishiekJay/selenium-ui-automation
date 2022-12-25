25e8eadac13c7c0230e1595008ca838649bf666c
Abishiek <abishiekjayan24@gmail.com>
2022-12-25 22:38:38 +0530

    Added Checkpoint class

--- src/test/java/org/abishiek/base/WebDriverFactory.java
+++ src/test/java/org/abishiek/base/WebDriverFactory.java
@@ -2,7 +2,10 @@ package org.abishiek.base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.CapabilityType;
 
 import java.time.Duration;
 
@@ -14,16 +17,18 @@ public class WebDriverFactory {
     public static WebDriverFactory getInstance(){
         return instance;
     }
-    private static ThreadLocal<WebDriver> threadedDriver = new ThreadLocal<WebDriver>();
+    private static ThreadLocal<WebDriver> threadedDriver = new ThreadLocal<>();
     public WebDriver getDriver(String browser){
         WebDriver driver;
         if(threadedDriver.get()==null){
             try{
                 if(browser.equalsIgnoreCase("chrome")){
-                    driver = new ChromeDriver();
+                    ChromeOptions options = setChromeOptions();
+                    driver = new ChromeDriver(options);
                     threadedDriver.set(driver);
                 }if(browser.equalsIgnoreCase("firefox")){
-                    driver = new FirefoxDriver();
+                    FirefoxOptions options = setFFOptions();
+                    driver = new FirefoxDriver(options);
                     threadedDriver.set(driver);
                 }
             }catch(Exception e){
@@ -38,4 +43,24 @@ public class WebDriverFactory {
         threadedDriver.get().quit();
         threadedDriver.set(null);
     }
+    /***
+     * Set Chrome Options
+     * @return options
+     */
+    private ChromeOptions setChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+        return options;
+    }
+
+    /***
+     * Set Firefox Options
+     * @return options
+     */
+    private FirefoxOptions setFFOptions() {
+        FirefoxOptions options = new FirefoxOptions();
+        options.setCapability(CapabilityType.ACCEPT_INSECURE_CERTS, false);
+        return options;
+    }
+
 }
